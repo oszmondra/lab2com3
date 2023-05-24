086cc376ed2d48037d9a6fb550f497b06a2d987b
EskinMH <eskinmontiel5@gmail.com>
2023-05-23 20:49:50 -0600

    Autenticación de usuarios

--- routes/web.php
+++ routes/web.php
@@ -16,13 +16,16 @@ use App\Http\Controllers\CategoryController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view ('auth.login');
+
+
 });
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route:: resource ("/projects", ProjectController::class); 
+Route:: resource ("/projects", ProjectController::class)->middleware('auth'); 
+
 
 
